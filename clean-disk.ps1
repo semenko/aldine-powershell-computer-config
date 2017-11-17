ce870dcec09fd5b82812ba45cb9e9710f133d8cf
Nick Semenkovich <nick@semenkovich.com>
2017-11-17 14:40:00 -0500

    Create clean-disk.ps1

---
+++ clean-disk.ps1
@@ -0,0 +1,25 @@
+# Set downloads to delete after 30 days
+
+
+# Run clean disk 
+# Via https://stackoverflow.com/questions/28852786/automate-process-of-disk-cleanup-cleanmgr-exe-without-user-intervention
+
+Write-Host 'Clearing CleanMgr.exe automation settings.'
+Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\*' -Name StateFlags0001 -ErrorAction SilentlyContinue | Remove-ItemProperty -Name StateFlags0001 -ErrorAction SilentlyContinue
+
+Write-Host 'Enabling Update Cleanup. This is done automatically in Windows 10 via a scheduled task.'
+New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Update Cleanup' -Name StateFlags0001 -Value 2 -PropertyType DWord
+
+Write-Host 'Enabling Temporary Files Cleanup.'
+New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Temporary Files' -Name StateFlags0001 -Value 2 -PropertyType DWord
+
+Write-Host 'Starting CleanMgr.exe...'
+Start-Process -FilePath CleanMgr.exe -ArgumentList '/sagerun:1' -WindowStyle Hidden -Wait
+
+Write-Host 'Waiting for CleanMgr and DismHost processes. Second wait neccesary as CleanMgr.exe spins off separate processes.'
+Get-Process -Name cleanmgr,dismhost -ErrorAction SilentlyContinue | Wait-Process
+
+$UpdateCleanupSuccessful = $false
+if (Test-Path $env:SystemRoot\Logs\CBS\DeepClean.log) {
+    $UpdateCleanupSuccessful = Select-String -Path $env:SystemRoot\Logs\CBS\DeepClean.log -Pattern 'Total size of superseded packages:' -Quiet
+}
