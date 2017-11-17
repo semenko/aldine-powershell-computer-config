d438813fef7d59f6b7dde28ca8d737e49fed160f
Nick Semenkovich <nick@semenkovich.com>
2017-11-17 15:15:00 -0500

    Create uninstall-default-apps.ps1

---
+++ uninstall-default-apps.ps1
@@ -0,0 +1,31 @@
+# Remove silly pre-installed apps
+
+Write-Host "Uninstalling cruft and Microsoft applications."
+Get-AppxPackage "Microsoft.3DBuilder" | Remove-AppxPackage
+Get-AppxPackage "Microsoft.Getstarted" | Remove-AppxPackage
+Get-AppxPackage "Microsoft.MicrosoftOfficeHub" | Remove-AppxPackage
+Get-AppxPackage "Microsoft.MinecraftUWP" | Remove-AppxPackage
+Get-AppxPackage "Microsoft.XboxApp" | Remove-AppxPackage
+Get-AppxPackage "Microsoft.XboxIdentityProvider" | Remove-AppxPackage
+Get-AppxPackage "Microsoft.XboxSpeechToTextOverlay" | Remove-AppxPackage
+Get-AppxPackage "Microsoft.XboxGameOverlay" | Remove-AppxPackage
+Get-AppxPackage "Microsoft.Xbox.TCUI" | Remove-AppxPackage
+Get-AppxPackage "9E2F88E3.Twitter" | Remove-AppxPackage
+Get-AppxPackage "king.com.CandyCrushSodaSaga" | Remove-AppxPackage
+Get-AppxPackage "4DF9E0F8.Netflix" | Remove-AppxPackage
+Get-AppxPackage "Drawboard.DrawboardPDF" | Remove-AppxPackage
+Get-AppxPackage "D52A8D61.FarmVille2CountryEscape" | Remove-AppxPackage
+Get-AppxPackage "GAMELOFTSA.Asphalt8Airborne" | Remove-AppxPackage
+Get-AppxPackage "flaregamesGmbH.RoyalRevolt2" | Remove-AppxPackage
+Get-AppxPackage "AdobeSystemsIncorporated.AdobePhotoshopExpress" | Remove-AppxPackage
+Get-AppxPackage "ActiproSoftwareLLC.562882FEEB491" | Remove-AppxPackage
+Get-AppxPackage "D5EA27B7.Duolingo-LearnLanguagesforFree" | Remove-AppxPackage
+Get-AppxPackage "Facebook.Facebook" | Remove-AppxPackage
+Get-AppxPackage "46928bounde.EclipseManager" | Remove-AppxPackage
+Get-AppxPackage "A278AB0D.MarchofEmpires" | Remove-AppxPackage
+Get-AppxPackage "KeeperSecurityInc.Keeper" | Remove-AppxPackage
+Get-AppxPackage "king.com.BubbleWitch3Saga" | Remove-AppxPackage
+Get-AppxPackage "89006A2E.AutodeskSketchBook" | Remove-AppxPackage
+Get-AppxPackage "CAF9E577.Plex" | Remove-AppxPackage
+
+Write-Host "Done."
