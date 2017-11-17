f0f59d209e1873836f4c596b14e88dcdd0f7c56d
Nick Semenkovich <nick@semenkovich.com>
2017-11-17 15:06:24 -0500

    Update computer-local-tweaks.ps1

--- computer-local-tweaks.ps1
+++ computer-local-tweaks.ps1
@@ -1,13 +1,28 @@
-## Assorted local tweaks and personal customizations
+## Assorted local site tweaks and customizations.
 
-# Polycom BToE Disable Popups & PC Audio
+## Enable NumLock
+# Derived from https://gist.github.com/alirobe/7f3b34ad89a159e6daa1
+Write-Host "Enabling NumLock after startup..."
+If (!(Test-Path "HKU:")) {
+  New-PSDrive -Name HKU -PSProvider Registry -Root HKEY_USERS | Out-Null
+}
+Set-ItemProperty -Path "HKU:\.DEFAULT\Control Panel\Keyboard" -Name "InitialKeyboardIndicators" -Type DWord -Value 2147483650
+Add-Type -AssemblyName System.Windows.Forms
+If (!([System.Windows.Forms.Control]::IsKeyLocked('NumLock'))) {
+  $wsh = New-Object -ComObject WScript.Shell
+  $wsh.SendKeys('{NUMLOCK}')
+}
 
-[HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Polycom\Polycom BToE Connector]
-"NOTIFY_MSG_EN"=dword:00000000
-"PLAYBACK_MODE_EN"=dword:00000000
-"WRN_MSGBOX_EN"=dword:00000000
 
-# GoToMyPC
+## Polycom BToE Disable Popups & PC Audio
+
+#[HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Polycom\Polycom BToE Connector]
+#"NOTIFY_MSG_EN"=dword:00000000
+#"PLAYBACK_MODE_EN"=dword:00000000
+#"WRN_MSGBOX_EN"=dword:00000000
+
+
+## GoToMyPC
 
 
 # 
