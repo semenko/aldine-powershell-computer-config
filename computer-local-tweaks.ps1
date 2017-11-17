9b38243378ad2f9d56da760f28ad5b5c119f49bf
Nick Semenkovich <nick@semenkovich.com>
2017-11-17 14:34:21 -0500

    Create computer-local-tweaks.ps1

---
+++ computer-local-tweaks.ps1
@@ -0,0 +1,13 @@
+## Assorted local tweaks and personal customizations
+
+# Polycom BToE Disable Popups & PC Audio
+
+[HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Polycom\Polycom BToE Connector]
+"NOTIFY_MSG_EN"=dword:00000000
+"PLAYBACK_MODE_EN"=dword:00000000
+"WRN_MSGBOX_EN"=dword:00000000
+
+# GoToMyPC
+
+
+# 
