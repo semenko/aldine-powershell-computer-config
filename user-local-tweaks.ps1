3514aff72f9856072808809cd7a9da4e65c4ae6c
Nick Semenkovich <nick@semenkovich.com>
2017-11-17 14:41:00 -0500

    Create user-local-tweaks.ps1

---
+++ user-local-tweaks.ps1
@@ -0,0 +1,5 @@
+# Popups
+
+[HKEY_CURRENT_USER\Software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge\New Windows\Allow]
+"book.princess.com"=hex:
+
