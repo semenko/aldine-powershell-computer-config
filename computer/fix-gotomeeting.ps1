e60f3a4b3bd51d3f598360adabe7fd85ccc45149
Nick Semenkovich <nick@semenkovich.com>
2017-11-18 02:00:23 -0500

    Update fix-gotomeeting.ps1

--- computer/fix-gotomeeting.ps1
+++ computer/fix-gotomeeting.ps1
@@ -1,4 +1,9 @@
 # Remove old G2M Runfiles
+Stop-Process -processname g2mstart
+Stop-Process -processname g2mlauncher
+Stop-Process -processname g2mcomm
+
+Remove-Item "C:\Program Files (x86)\Citrix\GoToMeeting" -Force -Recurse -ErrorAction Ignore
 
 Remove-ItemProperty -Path "HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Run" -Name "GoToMeetingInstall6956" -ErrorAction Ignore
 Remove-ItemProperty -Path "HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Run" -Name "GoToMeetingInstall7155" -ErrorAction Ignore
