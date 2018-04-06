6f3dcf0c252f160e7edf7ee10c7523e1b912272b
Nick Semenkovich <nick@semenkovich.com>
2018-04-06 14:43:53 -0400

    Update nxlog-configure.ps1

--- computer/nxlog-configure.ps1
+++ computer/nxlog-configure.ps1
@@ -5,12 +5,14 @@ $certURL = "https://raw.githubusercontent.com/semenko/aldine-powershell-computer
 
 Write-Output "Downloading nxlog config files."
 
-New-Item "C:\Program Files (x86)\nxlog\conf" -ItemType Directory
+New-Item "C:\Program Files (x86)\nxlog\conf" -ItemType Directory -ErrorAction SilentlyContinue
 
 (New-Object System.Net.WebClient).DownloadFile($configURL, "C:\Program Files (x86)\nxlog\conf\nxlog-aldine.conf")
 
+Copy-Item "C:\Program Files (x86)\nxlog\conf\nxlog-aldine.conf" "C:\Program Files (x86)\nxlog\conf\nxlog.conf" -ErrorAction SilentlyContinue
+Set-ItemProperty "C:\Program Files (x86)\nxlog\conf\nxlog.conf" -name IsReadOnly -value $true -ErrorAction SilentlyContinue
 
-New-Item "C:\Program Files (x86)\nxlog\cert" -ItemType Directory
+New-Item "C:\Program Files (x86)\nxlog\cert" -ItemType Directory -ErrorAction SilentlyContinue
 
 (New-Object System.Net.WebClient).DownloadFile($certURL, "C:\Program Files (x86)\nxlog\cert\papertrail-bundle.pem")
 
