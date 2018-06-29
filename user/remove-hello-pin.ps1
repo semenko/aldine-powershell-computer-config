54b5a4f1d30cd60c2772af83e95a64bc1bce0ab7
Nick Semenkovich <nick@semenkovich.com>
2018-06-29 19:58:51 -0400

    Delete remove-hello-pin.ps1

--- user/remove-hello-pin.ps1
+++
@@ -1,6 +0,0 @@
-# Fixes a Microsoft Intune bug
-
-Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Scope CurrentUser -Force
-Install-Module -Name Microsoft.WindowsPassportUtilities.Commands -Scope CurrentUser -Force
-Remove-PassportContainer –CurrentUser
