9432f0b059c288a0ca44a6d373e2f3463fb01893
Nick Semenkovich <nick@semenkovich.com>
2019-08-29 14:21:52 -0500

    Delete office-preferences.ps1

--- user/office-preferences.ps1
+++
@@ -1,4 +0,0 @@
-
-## Outlook
-# Show All Images in Emails
-New-ItemProperty -Path "HKCU:\Software\Microsoft\Office\16.0\Outlook\Options\Mail" -Name "BlockExtContent" -Value 0 -PropertyType DWORD -Force | Out-Null
