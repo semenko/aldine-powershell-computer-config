b93767096c35a131e761e3b2267d95fadd02d97c
Nick Semenkovich <semenko@alum.mit.edu>
2021-11-13 07:12:02 -0600

    Create remove-dell-cruft.ps1

---
+++ computer/remove-dell-cruft.ps1
@@ -0,0 +1,40 @@
+# Dell machines sometimes have pre-installed cruft:
+# - Support Assist
+# - Digital Delivery Services
+# - Dell Command
+
+# Let's remove them
+
+Start-Transcript -Path "C:\intune_remove-dell-cruft.log.txt"
+
+# Via https://stackoverflow.com/questions/56067312/how-to-uninstall-dell-support-assist-with-powershell
+
+# Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall  |
+#   Get-ItemProperty |
+#     Where-Object {$_.DisplayName -match "SupportAssist" -or $_.DisplayName -match "Dell Command" -or $_.DisplayName -match "Digital Delivery" } |
+#     Where-Object {$_.DisplayVersion -notlike "3.2*"} |
+#     Select-Object -Property  DisplayVersion, UninstallString, PSChildName
+
+# $SAVer = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall  |
+    Get-ItemProperty |
+        Where-Object {$_.DisplayName -match "SupportAssist" } |
+        Where-Object {$_.DisplayVersion -notlike "3.2*"} | 
+            Select-Object -Property DisplayVersion, UninstallString, PSChildName
+            
+$DellThings =  Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall  | 
+    Get-ItemProperty |
+        Where-Object {$_.DisplayName -ne $null -and $_.DisplayName.StartsWith("Dell") } |
+        Where-Object {$_.DisplayVersion -notlike "3.2*"} |
+            Select-Objec -Property DisplayName, DisplayVersion, UninstallString, PSChildName
+
+ForEach ($ver in $SAVer) {
+    Write-Host "Removing: ${$ver.DisplayName}" 
+    If ($ver.UninstallString) {
+        WRite-Host "  Version: ${$ver.DisplayVersion}"
+        $uninst = $ver.UninstallString
+        & cmd /c $uninst /quiet /norestart
+
+    }
+}
+
+Stop-Transcript
