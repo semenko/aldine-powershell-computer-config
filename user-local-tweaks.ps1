9dc0edb3c7b45bd374269127e89cb1b33ce98980
Nick Semenkovich <semenko@alum.mit.edu>
2021-11-13 06:49:34 -0600

    Delete user-local-tweaks.ps1

--- user-local-tweaks.ps1
+++
@@ -1,5 +0,0 @@
-# Popups
-
-[HKEY_CURRENT_USER\Software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge\New Windows\Allow]
-"book.princess.com"=hex:
-
